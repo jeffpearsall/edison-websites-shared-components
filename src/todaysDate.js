622ba077dd9533a1c236fdee3309881f4d47672d
Jeff Pearsall <jeffpearsall@squiber.com>
2019-09-26 15:39:19 -0700

    Update todaysDate.js

--- src/todaysDate.js
+++ src/todaysDate.js
@@ -1,4 +1,14 @@
-import React from "react";
-const TodaysDate = () =>
-  <div>{`Today's date is ${Date()}`}</div>;
-export default TodaysDate;
+import React, { Component } from 'react';
+
+export default class TodaysDate extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {};
+  }
+  render(){
+    let date = new Date();
+    return (
+      <div>{"Today's date is "+date}</div>
+    )
+  }
+}
